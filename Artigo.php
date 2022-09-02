fc04b4d4ff313e9b5e67d6991bd47279f5bbceb1
Antony Maziero Warken <againplayi7@gmail.com>
2022-09-02 16:45:51 -0300

    cap 2 vid 3 finalizado

--- Artigo.php
+++ Artigo.php
@@ -1,17 +1,24 @@
-<?php
-class Artigo {
+<!DOCTYPE html>
+<html lang="pt-br">
 
-  private $mysql;
-  public function __construct(mysqli $mysql) {
-    $this->mysql = $mysql;
-  }
-  public function exibirTodos(): array {
-    $resultado = $this->mysql->query('SELECT id, titulo, conteudo FROM artigos');
-    $artigos = $resultado->fetch_all(MYSQLI_ASSOC);
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-    return $artigos;
-  }
-}
+<body>
+    <div id="container">
+        <h1>
+            Título do artigo
+        </h1>
+        <p>
+            Conteúdo do artigo
+        </p>
+        <div>
+            <a class="botao botao-block" href="index.php">Voltar</a>
+        </div>
+    </div>
+</body>
 
-
- ?>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require 'config.php';
-include 'Artigo.php';
+include './src/Artigo.php';
 $artigo = new Artigo($mysql);
 $artigos = $artigo->exibirTodos();
 ?>
@@ -19,7 +19,7 @@ $artigos = $artigo->exibirTodos();
         <h1>Meu Blog</h1>
         <?php foreach($artigos as $artigo):?>
         <h2>
-            <a href="<?php echo $artigo['link']?>">
+            <a href="artigo.php?id=<?php echo $artigo['id']?>">
                 <?php echo $artigo['titulo']?>
             </a>
         </h2>
